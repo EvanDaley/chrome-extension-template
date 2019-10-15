bbfd981e6b0f0b062adb7436547faa0cf140fdfc
Evan Daley <evandaley@evandaley.office.lan>
2019-10-15 11:00:48 -0700

    Work in progress

--- src/js/popup.js
+++ src/js/popup.js
@@ -9,6 +9,27 @@
  *
  */
 
+// This port enables a long-lived connection to in-content.js
+let port = null;
+
+const sendPortMessage = message => port.postMessage(message);
+
+const messageHandler = message => {
+  console.log('popup.js - received message:', message);
+};
+
+// Find the current active tab
+const getTab = () =>
+  new Promise(resolve => {
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true
+      },
+      tabs => resolve(tabs[0])
+    );
+  });
+
 // Start the popup script, this could be anything from a simple script to a webapp
 const initPopupScript = () => {
   document.getElementById("searchForm").addEventListener('submit', function (event) {
@@ -18,31 +39,9 @@ const initPopupScript = () => {
 
   // Access the background window object
   const backgroundWindow = chrome.extension.getBackgroundPage();
+
   // Do anything with the exposed variables from background.js
   console.log(backgroundWindow.sampleBackgroundGlobal);
-
-  // This port enables a long-lived connection to in-content.js
-  let port = null;
-
-  // Send messages to the open port
-  const sendPortMessage = message => port.postMessage(message);
-
-  // Find the current active tab
-  const getTab = () =>
-    new Promise(resolve => {
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true
-        },
-        tabs => resolve(tabs[0])
-      );
-    });
-
-  // Handle port messages
-  const messageHandler = message => {
-    console.log('popup.js - received message:', message);
-  };
 
   // Find the current active tab, then open a port to it
   getTab().then(tab => {
@@ -55,11 +54,8 @@ const initPopupScript = () => {
   });
 };
 
-// Send a message to in-content.js
 const submitForm = () => {
   try {
-    chrome.extension.getBackgroundPage().console.log('here')
-
     const form = document.getElementById("searchForm");
     const elements = form.elements;
 
@@ -69,59 +65,16 @@ const submitForm = () => {
       data[item.name] = item.value;
     }
 
-    // Find the current active tab
-    const getTab = () =>
-      new Promise(resolve => {
-        chrome.tabs.query(
-          {
-            active: true,
-            currentWindow: true
-          },
-          tabs => resolve(tabs[0])
-        );
-      });
+    let messsage = {
+      type: 'SEARCH',
+      formData: data.search
+    }
 
-    const messageHandler = message => {
-      console.log('popup.js - received message:', message);
-    };
-
-    chrome.extension.getBackgroundPage().console.log('here3')
-    chrome.extension.getBackgroundPage().console.log(data)
-
-    // This port enables a long-lived connection to in-content.js
-    let port = null;
-
-    // Send messages to the open port
-    const sendPortMessage = message => port.postMessage(message);
-
-    // Find the current active tab, then open a port to it
-    getTab().then(tab => {
-      // Connects to tab port to enable communication with inContent.js
-      port = chrome.tabs.connect(tab.id, {name: 'chrome-extension-template'});
-      // Set up the message listener
-      port.onMessage.addListener(messageHandler);
-      // Send a test message to in-content.js
-
-      let messsage = {
-        type: 'SEARCH',
-        formData: 'asdfasdf'
-      }
-
-      sendPortMessage('asdfasdfa');
-    });
-
-    // chrome.runtime.sendMessage({
-    //   type: 'FORM_SUBMISSION',
-    //   formData: data
-    // })
-
-    chrome.extension.getBackgroundPage().console.log('here5')
-
+    sendPortMessage(messsage);
   } catch (err) {
     // This is necessary to unify error logs.
     chrome.extension.getBackgroundPage().console.log(err)
   }
-
 }
 
 
